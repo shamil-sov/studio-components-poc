6c1ea5df7ec5bcff85189f145bcc321621e01007
Shamil Sadiqov <shamil.sadiqov@bandlab.com>
2026-03-22 23:20:49 +0400

    Update

--- studio-breakdown/vite.config.ts
+++ studio-breakdown/vite.config.ts
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/studio-components-poc/studio-breakdown/',
+  base: '/studio-components-poc/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
